4080307681a383624cf805485192890b5d85fee8
dharikamal <76258348+dharikamal@users.noreply.github.com>
2020-12-20 19:28:36 +0300

    my cw1

---
+++ gamedevcw1/gamedevcw1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace gamedevcw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String CharName = "Ahmed";
+            int age = 15;
+            float Speed = 1.65f;
+            String superpower1 = "steel sheild";
+            String superpower2 = "Night vision";
+            Console.WriteLine("My name is {0}, My SuperPower is {1}, {2}, am {3} years old, my speed is {4}", CharName, superpower1, superpower2, age, Speed);
+        }
+    }
+}
