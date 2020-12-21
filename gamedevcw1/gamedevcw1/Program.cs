1e0ff139fa5dce9e2c72cde4b1412edc108ac462
dharikamal <76258348+dharikamal@users.noreply.github.com>
2020-12-21 17:47:51 +0300

    :)

--- gamedevcw1/gamedevcw1/Program.cs
+++ gamedevcw1/gamedevcw1/Program.cs
@@ -11,7 +11,18 @@ namespace gamedevcw1
             float Speed = 1.65f;
             String superpower1 = "steel sheild";
             String superpower2 = "Night vision";
+            string heroName = "aryax";
+            int Age = 21;
+            float speeds = 1.75f;
+            string Spower1 = "fire";
+            string Spower2 = "moving fast";
+            bool isBoss = true;
+
             Console.WriteLine("My name is {0}, My SuperPower is {1}, {2}, am {3} years old, my speed is {4}", CharName, superpower1, superpower2, age, Speed);
+            Console.WriteLine("My name is {0}, My SuperPower is {1}, {2}, am {3} years old, my speed is {4}", heroName, Spower1, Spower2, Age, speeds);
+
+            Console.WriteLine(age < Age);
+
         }
     }
 }
